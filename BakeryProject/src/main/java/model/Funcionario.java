7768bb10c0d0eb1e7a6f378b2fd860ba901eaf06
joaohneri <joaohnsousa@gmail.com>
2025-04-14 20:42:50 -0300

    Feat: Funcionário Herdando de Pessoa

--- BakeryProject/src/main/java/model/Funcionario.java
+++ BakeryProject/src/main/java/model/Funcionario.java
@@ -10,20 +10,13 @@ import java.time.LocalDate;
 /**
  *
  * @author joaoh
- * Classe Funcionário
+ * Classe Funcionário que herda de Pessoa
  */
 
 /**
- * Classe Funcionário
+ * Classe Funcionário que herda de Pessoa
  */
-public class Funcionario {
-    private Long id;
-    private String nome;
-    private String cpf;
-    private LocalDate dataNascimento;
-    private String endereco;
-    private String telefone;
-    private String email;
+public class Funcionario extends Pessoa {
     private String cargo;
     private String departamento;
     private double salario;
@@ -31,74 +24,10 @@ public class Funcionario {
     public Funcionario(Long id, String nome, String cpf, LocalDate dataNascimento, 
                        String endereco, String telefone, String email, 
                        String cargo, String departamento, double salario) {
-        this.id = id;
-        this.nome = nome;
-        this.cpf = cpf;
-        this.dataNascimento = dataNascimento;
-        this.endereco = endereco;
-        this.telefone = telefone;
-        this.email = email;
+        super(id, nome, cpf, dataNascimento, endereco, telefone, email);
         this.cargo = cargo;
         this.departamento = departamento;
         this.salario = salario;
-    }
-    
-    
-    public Long getId() {
-        return id;
-    }
-
-    // Getters and setters
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public LocalDate getDataNascimento() {
-        return dataNascimento;
-    }
-
-    public void setDataNascimento(LocalDate dataNascimento) {
-        this.dataNascimento = dataNascimento;
-    }
-
-    public String getEndereco() {
-        return endereco;
-    }
-
-    public void setEndereco(String endereco) {
-        this.endereco = endereco;
-    }
-
-    public String getTelefone() {
-        return telefone;
-    }
-
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 
     public String getCargo() {
@@ -124,5 +53,4 @@ public class Funcionario {
     public void setSalario(double salario) {
         this.salario = salario;
     }
-
 }
